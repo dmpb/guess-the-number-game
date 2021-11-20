f7a2ca8bab17877913f9c06c253553a73a18831a
Daniel Ponce <51546545+dmpb@users.noreply.github.com>
2021-11-19 20:01:25 -0500

    Create game

---
+++ public/script.js
@@ -0,0 +1,74 @@
+const number = document.getElementById("numberInput")
+const submitButton = document.getElementById("submitButton")
+const startNewGameButton = document.getElementById("startNewGameButton")
+const guessesText = document.getElementById("guesses")
+const lastResultText = document.getElementById("lastResult")
+const lowOrHiText = document.getElementById("lowOrHi")
+
+var intents = 1
+var hiddenNumber = newHiddenNumber()
+
+submitButton.addEventListener("click", submitGuess)
+startNewGameButton.addEventListener("click", startNewGame)
+
+function submitGuess() {
+    let guessNumber = Number(number.value)
+
+    if (intents === 1) {
+        guessesText.textContent = "Previous guesses:"
+    }
+
+    guessesText.textContent += " " + guessNumber
+
+    if (guessNumber === hiddenNumber) {
+        lastResultText.textContent = "Success!"
+        lastResultText.style.backgroundColor = "green"
+        lowOrHiText.textContent = ""
+        setGameOver()
+    } else if (intents === 10) {
+        lastResultText.textContent = "Game Over!"
+        lastResultText.style.backgroundColor = "red"
+        lowOrHiText.textContent = ""
+        setGameOver()
+    } else {
+        if (guessNumber > hiddenNumber) {
+            lastResultText.textContent = "Wrong!"
+            lastResultText.style.backgroundColor = "red"
+            lowOrHiText.textContent = "Last guess was too high!"
+            console.log("hight")
+        } else if (guessNumber < hiddenNumber) {
+            console.log("low")
+            lastResultText.textContent = "Wrong!"
+            lastResultText.style.backgroundColor = "red"
+            lowOrHiText.textContent = "Last guess was too low!"
+        }
+    }
+
+    intents++
+    number.value = ""
+}
+
+function startNewGame() {
+    intents = 1
+    guessesText.textContent = ""
+    lastResultText.textContent = ""
+    lowOrHiText.textContent = ""
+
+    lastResultText.style.backgroundColor = "white"
+    startNewGameButton.style.display = "none"
+
+    number.disabled = false
+    submitButton.disabled = false
+
+    hiddenNumber = newHiddenNumber()
+}
+
+function newHiddenNumber() {
+    return Math.floor(Math.random() * 10 + 1)
+}
+
+function setGameOver() {
+    number.disabled = true
+    submitButton.disabled = true
+    startNewGameButton.style.display = "block"
+}
